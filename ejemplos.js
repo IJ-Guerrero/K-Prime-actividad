613afff58535cbebb5a69878654bd5d2bfbe49c4
IJ-Guerrero <109167544+IJ-Guerrero@users.noreply.github.com>
2022-07-15 09:03:41 -0300

    20220715-1

---
+++ ejemplos.js
@@ -0,0 +1,74 @@
+function esPrimo(numero) {
+    for(let i = 2; i <= Math.sqrt(numero); ++i) {
+        if (numero % i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function obtenerFactoresPrimos(numero) {
+    if (typeof numero === 'number' && Number.isInteger(numero)) {
+        if (numero > 0) {
+            let primos = [];
+
+            for(let i = 2; i <= numero; ++i) {
+                while(esPrimo(i) && numero % i === 0) {
+                    primos.push(i);
+                    numero /= i;
+                }
+            }
+
+            return primos;
+        } else {
+            return null;
+        }
+    } else {
+        throw TypeError('El argumento debe ser un número entero.');
+    }
+}
+function countKprimes(k, start, nd) {
+    let respuesta = []
+    start = Math.abs(start)
+
+    if(start === 0){
+        start++
+    }
+
+    for(let i = start; i <= nd; ++i){
+        if(obtenerFactoresPrimos(i).length === k){
+             respuesta.push(i) 
+        }
+        
+    }
+    return respuesta;
+}
+
+function puzzle(s){
+    let sevenPrime = countKprimes(7, 0, s)
+    let threePrime = []
+    let onePrime = []
+    
+    countKprimes(3, 0, s).forEach( function(valor, indice) {
+        if(valor < s - countKprimes(7, 0, s)[0]){
+            threePrime.push(valor)
+        }
+    })
+
+    countKprimes(1, 0, s).forEach( function(valor, indice) {
+        if(valor < s - countKprimes(7, 0, s)[0]){
+            onePrime.push(valor)
+        }
+    })
+    
+
+
+    return [sevenPrime, 
+        threePrime,
+        onePrime]
+
+    };
+console.log(puzzle(143))
+console.log(esPrimo(5));
+console.log(130 - countKprimes(7, 0, 143)[0]);
