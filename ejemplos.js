6101a9bb26f34f0340978fa9d7a0c22719690d02
IJ-Guerrero <109167544+IJ-Guerrero@users.noreply.github.com>
2022-07-15 20:07:58 -0300

    respuestaYOtrasActividades

--- ejemplos.js
+++ ejemplos.js
@@ -80,7 +80,53 @@ function puzzle(s){
 
     };
 
+function even_or_odd(number) {
+    if (number === 0){
+        return "Even"
+    } else if(number % 2 === 0){
+        return "Even"
+    } else {
+        return "Odd"
+    }
+    }
 
+function Mayor (array) {
+    let NMayor = 0;
+
+    array.forEach(function(valor){
+        if (valor >= NMayor){
+            NMayor = valor
+        }
+    })
+    return NMayor;
+}
+
+function divideEnCinco (letras) {
+    let numeros = 0
+    for(i = 0; i <= letras.length ; ++i){
+        numeros = Math.max(numeros, (letras.slice(i, i + 5)))
+    }
+
+    return numeros;
+}
+
+function solution(digits){
+    let numeros = 0
+    for(i = 0; i <= digits.length ; ++i){
+        numeros = Math.max(numeros, (digits.slice(i, i + 5)))
+    }
+
+    return numeros;
+}
+
+function fibonacci(n) {
+    if(n==0 || n == 1)
+        return n;
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+let c = "hola"
+console.log(fibonacci(5))
 console.log(puzzle(143))
-console.log(esPrimo(5));
-console.log(130 - countKprimes(7, 0, 143)[0]);
+console.log(Array.from(new Set([6,7,8,8,8])) );
+console.log(c.slice(c.length - c.length, 1));
+console.log(solution("7316717653133062491922511967442657474235534919493496983520368542506326239578318016984801869478851843858615607891129494954595017379583319528532"))
